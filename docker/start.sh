d65a5f67c06dd976819e272da96c57a1dbd54472
Fredrik Thulin <fredrik@thulin.net>
2017-04-20 14:02:13 +0200

    remove some eduids

--- docker/start.sh
+++ docker/start.sh
@@ -6,7 +6,7 @@ set -x
 # These could be set from Puppet if multiple instances are deployed
 sunetfrontend_name=${sunetfrontend_name-'sunetfrontend-api'}
 app_name=${app_name-'api'}
-base_dir=${base_dir-"/opt/eduid"}
+base_dir=${base_dir-"/opt/sunetfrontend"}
 project_dir=${project_dir-"${base_dir}/${sunetfrontend_name}"}
 app_dir=${app_dir-"${project_dir}/${app_name}"}
 # These *can* be set from Puppet, but are less expected to...
@@ -16,15 +16,17 @@ workers=${workers-1}
 worker_class=${worker_class-sync}
 worker_threads=${worker_threads-1}
 worker_timeout=${worker_timeout-30}
+runas_user=${runas_user-'sunetfrontend'}
+runas_group=${runas_group-'sunetfrontend'}
 
-chown -R eduid: "${log_dir}" "${state_dir}"
-test -d /backends && chown -R eduid: /backends
+chown -R ${runas_user}:${runas_group} "${log_dir}" "${state_dir}" || true
+test -d /backends && chown -R ${runas_user}:${runas_group} /backends || true
 
 # set PYTHONPATH if it is not already set using Docker environment
 export PYTHONPATH=${PYTHONPATH-${project_dir}}
 
 extra_args=""
-if [ -d "/opt/eduid/src/sunet-frontend-api/sunetfrontend" ]; then
+if [ -d "${base_dir}/src/sunet-frontend-api/sunetfrontend" ]; then
     # developer mode, restart on code changes
     extra_args="--reload"
 fi
@@ -35,7 +37,7 @@ fi
 # version of something is actually running.
 pip freeze
 
-exec start-stop-daemon --start -c eduid:eduid --exec \
+exec start-stop-daemon --start -c ${runas_user}:${runas_group} --exec \
      ${base_dir}/bin/gunicorn \
      --pidfile "${state_dir}/${sunetfrontend_name}.pid" \
      -- \
