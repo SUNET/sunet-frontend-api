fd711f6b98a8586326f3dc7d1c317f4a901bf972
Fredrik Thulin <fredrik@thulin.net>
2017-04-20 13:37:01 +0200

    init

---
+++ docker/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Install requirements that can be cached between docker builds
+#
+
+set -e
+set -x
+
+apt-get update
+apt-get -y dist-upgrade
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+PYPI="https://pypi.sunet.se/simple/"
+ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
+
+echo "#############################################################"
+echo "$0: Using PyPi URL ${PYPI}"
+echo "#############################################################"
+
+/opt/eduid/bin/pip freeze
