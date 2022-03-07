3bc8098d088017f274b3f7495e7d1c783ea05516
Fredrik Thulin <fredrik@thulin.net>
2022-03-07 21:11:34 +0100

    log to stdout

--- docker/start.sh
+++ docker/start.sh
@@ -10,7 +10,6 @@ base_dir=${base_dir-"/opt/eduid"}
 project_dir=${project_dir-"${base_dir}/${sunetfrontend_name}"}
 app_dir=${app_dir-"${project_dir}/${app_name}"}
 # These *can* be set from Puppet, but are less expected to...
-log_dir=${log_dir-'/var/log/sunetfrontend-api'}
 state_dir=${state_dir-"${base_dir}/run"}
 workers=${workers-1}
 worker_class=${worker_class-sync}
@@ -19,7 +18,7 @@ worker_timeout=${worker_timeout-30}
 runas_user=${runas_user-'sunetfrontend'}
 runas_group=${runas_group-'sunetfrontend'}
 
-chown -R ${runas_user}:${runas_group} "${log_dir}" "${state_dir}" || true
+chown -R ${runas_user}:${runas_group} ${state_dir}" || true
 test -d /backends && chown -R ${runas_user}:${runas_group} /backends || true
 
 # set PYTHONPATH if it is not already set using Docker environment
@@ -44,6 +43,5 @@ exec start-stop-daemon --start -c ${runas_user}:${runas_group} --exec \
      --bind 0.0.0.0:8080 \
      --workers ${workers} --worker-class ${worker_class} \
      --threads ${worker_threads} --timeout ${worker_timeout} \
-     --access-logfile "${log_dir}/${sunetfrontend_name}-access.log" \
-     --error-logfile "${log_dir}/${sunetfrontend_name}-error.log" \
+     --capture-output \
      ${extra_args} sunetfrontend.run:app
